6bb70be92502d18aecd512d63b48efa3a0fa1088
Ignacio Martina <ignacioamartina@gmail.com>
2026-06-13 10:05:02 -0300

    Sitio web GamerZone con imágenes y pruebas actualizadas

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path'); 
 const app = express();
 const port = 3000;
 
+// Le damos permiso a Express para mostrar los archivos de la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Cuando alguien entra a la raíz ("/"), le mandamos el archivo index.html
 app.get('/', (req, res) => {
-  res.send('¡Hola! La API de nuestro eCommerce está en línea y testeada.');
+  res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
 app.listen(port, () => {
-  console.log(`Aplicación de prueba escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
